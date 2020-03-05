bb291b1797949281147a009bc26061c7856a7896
Simon Mallet <simon.mallet@globaleagle.com>
2020-03-05 13:28:08 +1300

    Advent day 3 part 1

---
+++ app/AdventDay3.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App;
+
+class AdventDay3
+{
+    /**
+     * Receive a wire's path and return visited nodes
+     * Format: R8,U5,L5,D3
+     *
+     * @see https://adventofcode.com/2019/day/3
+     * @param string $wirePath
+     * @return array
+     */
+    public static function fuelManagement(string $wirePath): array
+    {
+        $currentPositionX = 0;
+        $currentPositionY = 0;
+        $visited = [];
+        $moveRight = function($spaces) use (&$visited, &$currentPositionX, &$currentPositionY) {
+            for ($i = 1; $i <= $spaces; $i++) {
+                $currentPositionX++;
+                $visited["{$currentPositionX}.{$currentPositionY}"] = true;
+            }
+        };
+
+        $moveUp = function($spaces) use (&$visited, &$currentPositionX, &$currentPositionY) {
+            for ($i = 1; $i <= $spaces; $i++) {
+                $currentPositionY++;
+                $visited["{$currentPositionX}.{$currentPositionY}"] = true;
+            }
+        };
+
+        $moveLeft = function($spaces) use (&$visited, &$currentPositionX, &$currentPositionY) {
+            for ($i = 1; $i <= $spaces; $i++) {
+                $currentPositionX--;
+                $visited["{$currentPositionX}.{$currentPositionY}"] = true;
+            }
+        };
+
+        $moveDown = function($spaces) use (&$visited, &$currentPositionX, &$currentPositionY) {
+            for ($i = 1; $i <= $spaces; $i++) {
+                $currentPositionY--;
+                $visited["{$currentPositionX}.{$currentPositionY}"] = true;
+            }
+        };
+
+        $operations = explode(',', $wirePath);
+        foreach ($operations as $operation) {
+            $operationMoves = substr($operation, 1);
+            switch ($operation[0]) {
+                case 'R':
+                    $moveRight($operationMoves);
+                    break;
+                case 'U':
+                    $moveUp($operationMoves);
+                    break;
+                case 'L':
+                    $moveLeft($operationMoves);
+                    break;
+                case 'D':
+                    $moveDown($operationMoves);
+                    break;
+            }
+        }
+        return $visited;
+    }
+}
